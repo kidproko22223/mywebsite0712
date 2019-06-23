162854b5710b848845eef61c8dcb46c233c96e4f
kidproko2223 <44961169+kidproko2223@users.noreply.github.com>
2019-06-23 09:07:35 +0700

    add

---
+++ CP/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Toys Store</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/owl.carousel.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</head>
+<body>
+	<!--header-->
+		<div class="header">
+			<div class="header-top">
+			<div class="container">
+				 <div class="lang_list">
+					<select tabindex="4" class="dropdown1">
+						<option value="" class="label" value="">En</option>
+						<option value="1">English</option>
+						<option value="2">French</option>
+						<option value="3">German</option>
+					</select>
+   			</div>
+				<div class="top-right">
+				<ul>
+					<li class="text"><a href="login.html">login</a></li>
+					<li><div class="cart box_1">
+							<a href="login.php">
+								 <span class="simpleCart_total"> $0.00 </span> (<span id="simpleCart_quantity" class="simpleCart_quantity"> 0 </span>)
+							</a>	
+							<p><a href="javascript:;" class="simpleCart_empty">Empty cart</a></p>
+							<div class="clearfix"> </div>
+						</div></li>
+				</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			</div>
+			<div class="header-bottom">
+					<div class="container">
+<!--/.content-->
+<div class="content white">
+	<nav class="navbar navbar-default" role="navigation">
+	    <div class="navbar-header">
+	        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	        </button>
+	        <h1 class="navbar-brand"><a  href="index.html">Toys Store</a></h1>
+	    </div>
+	    <!--/.navbar-header-->
+	
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	        <ul class="nav navbar-nav">
+			<li><a href="index.php">Home</a></li>
+		        <li class="dropdown">
+		        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">kids <b class="caret"></b></a>
+		            <ul class="dropdown-menu multi-column columns-3">
+			            <div class="row">
+				            <div class="col-sm-4">
+					             <ul class="multi-column-dropdown">
+						            <li><a class="list" >Kids</a></li>
+						            <li><a class="list1">Card</a></li>
+						            <li><a class="list1">Model toys</a></li>
+						            <li><a class="list1">Assembled toys</a></li>
+						            <li><a class="list1">Educational toys</a></li>
+					            </ul>
+				            </div>
+			        </ul>
+                </li>
+                <li>
+                <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+                </li>
+                <li>
+                <a href="InsertData.php" target="_blank" >Insert </a>
+                </li>
+                <li>
+                <a href="UpdateData.php" target="_blank" >Update </a> 
+                </li>
+                <li>
+                <a href="DeleteData.php" target="_blank" >Delete </a> 
+                </li>
+				<li><a>Top</a></li>
+		    </ul>
+	    </div>
+	  
+		<!--header-->
+		<div class="banner-section">
+			<div class="container">
+				<div class="banner-grids">
+					<div class="col-md-6 banner-grid">
+						<h2>Welcome to Toys Store</h2>  
+						<p>Prestige, Quality, Safety guarantee</p>
+						<a class="button"> shop now </a>
+					</div>
+				<div class="col-md-6 banner-grid1">
+						<img src="E:\Assignment 2\Swim wear\CP\images\image1.jpg" class="img-responsive" alt=""/>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		</div>
+		<div class="banner-bottom">
+		<div class="gallery-cursual">
+		<!--sreen-gallery-cursual-->
+		</div>
+		</div>
+		<div class="gallery">
+			<div class="container">
+			<h3>Featured products</h3>
+			<div class="gallery-grids">
+				<div class="col-md-3 gallery-grid ">
+					<img src="E:\Assignment 2\Swim wear\CP\images\image2.jpg" class="img-responsive" alt=""/>
+					<div class="gallery-info">
+					<div class="quick">
+					<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+					</div>
+					</div>
+					<div class="galy-info">
+						<p>Robot</p>
+						<div class="galry">
+						<div class="prices">
+						<h5 class="item_price">$95.00</h5>
+						</div>
+					<div class="rating">
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+								</div>
+						
+					<div class="clearfix"></div>
+					</div>
+					</div>
+				</div>
+				<div class="col-md-3 gallery-grid">
+						<img src="E:\Assignment 2\Swim wear\CP\images\image3.jpg" class="img-responsive" alt=""/>
+						<div class="gallery-info">
+					<div class="quick">
+					<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+					</div>
+					</div>
+					<div class="galy-info">
+						<p>Pirate</p>
+						<div class="galry">
+						<div class="prices">
+						<h5 class="item_price">$25.00</h5>
+						</div>
+					<div class="rating">
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+								</div>
+						
+					<div class="clearfix"></div>
+					</div>
+					</div>
+				</div>
+					<div class="col-md-3 gallery-grid">
+						<img src="E:\Assignment 2\Swim wear\CP\images\image4.jpg" class="img-responsive" alt=""/>
+						<div class="gallery-info">
+					<div class="quick">
+					<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+					</div>
+					</div>
+					<div class="galy-info">
+						<p>Truck</p>
+						<div class="galry">
+						<div class="prices">
+						<h5 class="item_price">$10.00</h5>
+						</div>
+					<div class="rating">
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+								</div>
+						
+					<div class="clearfix"></div>
+					</div>
+					</div>
+				</div>
+				<div class="col-md-3 gallery-grid">
+						<img src="E:\Assignment 2\Swim wear\CP\images\image5.jpg" class="img-responsive" alt=""/>
+						<div class="gallery-info">
+					<div class="quick">
+					<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+					</div>
+					</div>
+					<div class="galy-info">
+						<p>Bingo</p>
+						<div class="galry">
+						<div class="prices">
+						<h5 class="item_price">$35.00</h5>
+						</div>
+					<div class="rating">
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+									<span>☆</span>
+								</div>
+						
+					<div class="clearfix"></div>
+					</div>
+					</div>
+				</div>
+					<div class="col-md-4 footer-grid1">
+					<div class="social-icons">
+						<a href="#"><i class="icon"></i></a>
+						<a href="#"><i class="icon1"></i></a>
+						<a href="#"><i class="icon2"></i></a>
+						<a href="#"><i class="icon3"></i></a>
+						<a href="#"><i class="icon4"></i></a>
+					</div>
+					<p>Copyright &copy; 2019 Toys Store.</p>
+					</div>
+				<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+
+</body>
+</html>
